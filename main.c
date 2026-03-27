971dfd9db2dd51a52800ea967f7149e00860c41b
Aleksandar Kolev <akolev287@gmail.com>
2026-03-27 16:12:33 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define MAX_LINES 1000
+#define MAX_COLS 256
+
+typedef struct {
+    char lines[MAX_LINES][MAX_COLS];
+    int num_lines;
+    int cursor_row;
+    int cursor_col;
+    char filename[256];
+    int state
+} Editor;
+
+struct termios orig_termios;
+
+void disable_raw_mode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disable_raw_mode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
